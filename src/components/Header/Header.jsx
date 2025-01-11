a8cf918f89a5ada4faf1e6fd4591327bc1a19318
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-01-11 17:22:08 +0530

    Addes navbarcomponent using reactStrap:
    https://reactstrap.github.io/?path=/docs/components--navbar-toggler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-// library import
- 
+import { useState } from "react";
+
 import {
   Collapse,
   Navbar,
@@ -11,46 +11,37 @@ import {
   DropdownMenu,
   DropdownItem,
   NavbarText,
-} from 'reactstrap';
-
+} from "reactstrap";
 
 // CSS import
-import './Header.css';
-// Context import
-
-
+import "./Header.css";
 
 function Header(props) {
   const [isOpen, setIsOpen] = useState(false);
-  
+
   const toggle = () => setIsOpen(!isOpen);
-  
-  
 
   return (
     <div>
       <Navbar {...props}>
-        <NavbarBrand id="title">
-          <Link to="/">ZapKart</Link>
-
-        </NavbarBrand>
+        <NavbarBrand id="title">ZapKart</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ms-auto" navbar>
-            <UncontrolledDropdown nav inNavbar style={{marginRight: '2rem'}}>
+            <UncontrolledDropdown nav inNavbar style={{ marginRight: "2rem" }}>
               <DropdownToggle nav caret>
                 Options
               </DropdownToggle>
               <DropdownMenu right>
-              <DropdownItem>Cart</DropdownItem>
+                {" "}
+                {/* Updated from right to end */}
+                <DropdownItem>Cart</DropdownItem>
                 <DropdownItem>Settings</DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>
-                  LogOut
-                </DropdownItem>
+                <DropdownItem>LogOut</DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
-             <NavbarText>UserName</NavbarText>
+            <NavbarText>UserName</NavbarText>
           </Nav>
         </Collapse>
       </Navbar>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// CSS imports
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
