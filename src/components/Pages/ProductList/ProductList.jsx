4dbc2a8f6cc8731a9138c45b5c07a11e8ec8f66a
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-01-12 02:43:09 +0530

    Added filter component to product list

--- src/components/Pages/ProductList/ProductList.jsx
+++ src/components/Pages/ProductList/ProductList.jsx
@@ -1,4 +1,5 @@
 // CSS import
+import FilterProduct from "../../FilterProduct/FilterProduct";
 import ProductBox from "../../ProductBox/ProductBox";
 import "./ProductList.css";
 
@@ -10,9 +11,11 @@ function ProductList() {
       <div className="row">
         <h2 className="product-list-title text-center">All Products</h2>
         <div className="product-list-wrapper d-flex flex-row">
+
+            <FilterProduct/>
           {/* list of products */}
           <div className="product-list-box" id="productList">
-           <ProductBox/>
+           <ProductBox productImage={productImage} name={"dummy"} price={1000}/>
           </div>
         </div>
       </div>
