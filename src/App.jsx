3fa65f9d690d5f112d0fb713f6e1779fe0e4a380
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-01-12 00:20:45 +0530

    added home page ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Error from './components/Pages/Error/Error'
+import Home from './components/Pages/Home/Home'
 
 function App() {
   
@@ -15,6 +17,9 @@ function App() {
         expand="md" 
         container="md"
       />
+
+      <Home/>
+      {/* <Error/> */}
 
      
 
