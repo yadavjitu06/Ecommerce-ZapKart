cc346b19f07acd102aa6937b091465b60b358716
yadavjitu06 <120108392+yadavjitu06@users.noreply.github.com>
2025-01-12 02:12:51 +0530

    Added product box card to product List Page

--- src/components/Pages/ProductList/ProductList.jsx
+++ src/components/Pages/ProductList/ProductList.jsx
@@ -1,38 +1,23 @@
-
-
 // CSS import
-import './ProductList.css';
+import ProductBox from "../../ProductBox/ProductBox";
+import "./ProductList.css";
 
 // Component import
 
-
-
-
-
 function ProductList() {
-
-    
-    return (
-        <div className='container'>
-            <div className='row'>
-                <h2 className='product-list-title text-center'>All Products</h2>
-                <div className='product-list-wrapper d-flex flex-row'>
-
-
-                    
-
-                    {/* list of products */}
-                    <div className='product-list-box' id='productList'>
-
-                       
-
-                    </div>
-
-
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <h2 className="product-list-title text-center">All Products</h2>
+        <div className="product-list-wrapper d-flex flex-row">
+          {/* list of products */}
+          <div className="product-list-box" id="productList">
+           <ProductBox/>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ProductList;
